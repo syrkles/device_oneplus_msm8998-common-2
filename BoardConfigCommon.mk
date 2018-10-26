1b8bc2db1e3861b1c5baaf363e4cc9ccc9c7634e
syrkles <syrkles@gmail.com>
2018-10-26 04:06:08 +0000

    clang time

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -82,7 +82,8 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8998
 TARGET_KERNEL_CONFIG := lineage_oneplus5_defconfig
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := 7.0.2
 
 # Enable real time lockscreen charging current values
 BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
